5a26dec8c63df9330e31bd7ab745a04ad886776c
Alexandre B <alexandrebender.ar@gmail.com>
2022-08-17 20:52:30 -0300

    Começando a implementar o método que imprime o tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿using tabuleiro;
 
-Tabuleiro Tab = new Tabuleiro(8, 8);
+Tabuleiro tab = new Tabuleiro(8, 8);
+
+Tela.imprimirTabuleiro(tab);
 
 Console.ReadLine();
+
+int[,] mat = new int[8,8];
+
+for (int i = 0; i<8; i++)
+{
+    for (int j = 0; j<8; j++)
+    {
+        Console.WriteLine(mat[i, j]);
+    }
+}
 
