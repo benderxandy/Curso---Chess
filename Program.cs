d49fc2c623a6c890c071cec6807c8d89f6e13b50
Alexandre B <alexandrebender.ar@gmail.com>
2022-08-17 21:24:14 -0300

    Começando a colocar pecas no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
 ﻿using tabuleiro;
+using xadrez;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
 Tela.imprimirTabuleiro(tab);
 
 Console.ReadLine();
-
-int[,] mat = new int[8,8];
-
-for (int i = 0; i<8; i++)
-{
-    for (int j = 0; j<8; j++)
-    {
-        Console.WriteLine(mat[i, j]);
-    }
-}
-
