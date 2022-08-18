e60806068dc23cd9178160fe14866980c2d616f6
Alexandre B <alexandrebender.ar@gmail.com>
2022-08-18 19:58:15 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -2,23 +2,23 @@
 using xadrez;
 
 
-PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+// PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-Console.WriteLine(pos);
+// Console.WriteLine(pos);
 
-Console.WriteLine(pos.toPosicao()); 
-// try {
-//     Tabuleiro tab = new Tabuleiro(8, 8);
+// Console.WriteLine(pos.toPosicao()); 
+try {
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
-//     tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-//     tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-//     tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
 
-//     Tela.imprimirTabuleiro(tab);
+    Tela.imprimirTabuleiro(tab);
 
-//     Console.ReadLine();
-// }
-// catch (tabuleiroException e)
-// {
-//     Console.WriteLine(e.Message);
-// }
+    Console.ReadLine();
+}
+catch (tabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
