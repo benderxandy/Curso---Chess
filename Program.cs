b9a1ec57255381add62637f077bd471e85b54251
Alexandre B <alexandrebender.ar@gmail.com>
2022-08-19 20:04:41 -0300

    Fazendo os primeiros testes de movimentacao

--- Program.cs
+++ Program.cs
@@ -8,15 +8,23 @@ using xadrez;
 
 // Console.WriteLine(pos.toPosicao()); 
 try {
-    Tabuleiro tab = new Tabuleiro(8, 8);
+    PartidaXadrez partida = new PartidaXadrez();
 
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+    while (!partida.terminada)
+    {
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab);
 
-    Tela.imprimirTabuleiro(tab);
+        Console.Write("Origem: ");
+        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+        Console.Write("Destino: ");
+        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-    Console.ReadLine();
+        partida.executaMovimento(origem, destino);
+    }
+
+
+
 }
 catch (tabuleiroException e)
 {
