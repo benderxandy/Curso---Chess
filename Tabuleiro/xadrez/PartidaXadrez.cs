abd822a61042793935a9b70837f1c600b3af5249
Alexandre B <alexandrebender.ar@gmail.com>
2022-08-31 20:43:22 -0300

    Jogada especial Promocao

--- Tabuleiro/xadrez/PartidaXadrez.cs
+++ Tabuleiro/xadrez/PartidaXadrez.cs
@@ -145,6 +145,22 @@ namespace xadrez
                 throw new tabuleiroException("Você não pode se colocar em cheque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //Jogada especial Promocao
+
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -162,8 +178,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //Jogada especial - En Passant
 
